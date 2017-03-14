9c981118a20f6c1b9eed0fee48c15dd5e8709a64
finsa anugrah pratama <finsambam@gmail.com>
2017-03-15 06:59:29 +0700

    add pdf generator

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,6 +5,7 @@ class ResultsController < ApplicationController
   end
 
   def get_analytics
+    session[:params] = params
     @criteria = Criterium.all
     @selected_criterium = Criterium.find(params[:criterium])
     @parameter_categories = ParameterCategory.all
@@ -17,15 +18,15 @@ class ResultsController < ApplicationController
 
   def save_analytic_as_pdf
     @criteria = Criterium.all
-    @selected_criterium = Criterium.find(params[:criterium])
+    @selected_criterium = Criterium.find(session[:params]["criterium"])
     @parameter_categories = ParameterCategory.all
-    @analytics = Analytic.by_river_name_and_period(params[:river_name], params[:period_date])
+    @analytics = Analytic.by_river_name_and_period(session[:params]["river_name"], session[:params]["period_date"])
 
     respond_to do |format|
       format.pdf do
         render pdf: 'file_name',
                template: 'results/analytics.pdf.erb',
-               layout: 'pdf'
+               layout: 'pdf',
                footer: {
                  center: 'Center',
                  left: 'Left',

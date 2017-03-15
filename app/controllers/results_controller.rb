0309395f994283476a049efdaaeb04b4a582a79c
finsa anugrah pratama <finsambam@gmail.com>
2017-03-16 03:56:35 +0700

    fix download pdf

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -21,12 +21,11 @@ class ResultsController < ApplicationController
     @selected_criterium = Criterium.find(session[:params]["criterium"])
     @parameter_categories = ParameterCategory.all
     @analytics = Analytic.by_river_name_and_period(session[:params]["river_name"], session[:params]["period_date"])
-
     respond_to do |format|
       format.pdf do
         render pdf: 'file_name',
                template: 'results/analytics.pdf.erb',
-               layout: 'pdf',
+               layout: 'layouts/pdf.html.erb',
                footer: {
                  center: 'Center',
                  left: 'Left',

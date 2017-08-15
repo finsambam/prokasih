b99b3b2046d10bad8ee53be3340a8d8c372e8fac
finsa anugrah pratama <finsambam@gmail.com>
2017-08-16 00:50:37 +0700

    update discussion feature

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,19 +1,57 @@
 class DiscussionsController < ApplicationController
+  before_filter :authenticate_user!, only: [:destroy]
+  before_filter :find_discussion, only: [:edit, :update]
+
   def index
     session[:offset] = 0
     get_data(session[:offset])
   end
 
   def create
-    @comment = Comment.new(name: params[  :name], email: params[:email], message: params[:message])
-    @comment.parent_id = params[:parent_id] if params[:parent_id].present?
-    if @comment.save!
-      @response = {success: true}
-      job_params = {type: "discuss", comment: @comment}
-      SendEmailJob.set(wait: 10.seconds).perform_later(job_params)
-    else
-      @response = {success: false, error: "Terjadi kesalahan pada saat penyimpanan data, silakan coba lagi"}
+    begin
+      @comment = Comment.new(name: params[:name], email: params[:email], message: params[:message])
+      @comment.parent_id = params[:parent_id] if params[:parent_id].present?
+      if @comment.save!
+        @response = {success: true}
+        job_params = {type: "discuss", comment: @comment}
+        SendEmailJob.set(wait: 10.seconds).perform_later(job_params)
+      else
+        @response = {success: false, error: "Terjadi kesalahan pada saat penyimpanan data, silakan coba lagi"}
+      end
+        
+    rescue Exception => e
+      @response = {success: false, comment: @comment}
     end
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def edit
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def update
+    begin
+      @comment.message = params[:message]
+      if @comment.save!
+        @response = {success: true}
+      else
+        @response = {success: false, error: "Terjadi kesalahan pada saat penyimpanan data, silakan coba lagi"}
+      end
+    rescue Exception => e
+      @response = {success: false, comment: @comment}
+    end
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def destroy
+    @comment = Comment.destroy(params[:id])
     respond_to do |format|
       format.js
     end
@@ -31,5 +69,9 @@ class DiscussionsController < ApplicationController
   def get_data(offset)
     limit = 5
     @comments = Comment.isParent().order('created_at DESC').limit(limit).offset(offset * limit)
+  end
+
+  def find_discussion
+    @comment = Comment.find(params[:id])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,17 @@ class Comment < ApplicationRecord
   belongs_to :parent, :class_name => "Comment", :foreign_key => "parent_id"
   has_many :childs, :class_name => "Comment", :foreign_key => "parent_id"
 
+  validate :must_be_presents
+  
   scope :isParent, -> { where(:parent_id => nil) }
+
+  def must_be_presents
+  	errors[:base] << "Nama tidak boleh kosong" if name.blank?
+    errors[:base] << "Email tidak boleh kosong" if email.blank?
+    errors[:base] << "Pesan tidak boleh kosong" if message.blank?
+  end
+
+  def check_email_format
+  	
+  end
 end
